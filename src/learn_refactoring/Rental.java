87fc39afb683abdec89b7388562cbd43ae44fe86
kondo takumi <s.093.kondo@gmail.com>
2020-01-23 19:39:03 +0900

    first commit

---
+++ src/learn_refactoring/Rental.java
@@ -0,0 +1,15 @@
+package learn_refactoring;
+
+public class Rental {
+
+    private Movie _movie;
+    private int _daysRened;
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    public int getDaysrented() {
+        return _daysRened;
+    }
+}
